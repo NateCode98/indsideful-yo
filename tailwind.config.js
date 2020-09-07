08937b3555d5104bc03d9478527e275ae5da8f85
Nate <jonathankuhl@outlook.com>
2020-09-07 16:24:27 +0100

    ff

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  theme: {
+    container: {
+      padding: '1.5rem',
+    },
+    fontFamily: {
+      display: ['Montserrat', 'sans-serif'],
+      body: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      fontSize: {
+        '7xl': '4.5rem',
+      },
+      margin: {
+        '-72': '-18rem',
+        '-100': '-25rem',
+      },     
+      spacing: {
+        pc80: '80%',
+      },
+      backgroundColor: {
+        bgOpacity: 'rgba(26, 32, 44, 0.9)'
+      },
+      inset: {
+        '1' : '1rem',
+        '2' : '2rem',
+        '3' : '3rem',
+        '4' : '4rem',
+        '5' : '5rem',
+        '-8': '-2rem',
+        '-22': '-22rem',
+      },
+      maxWidth: {
+        '38r': '38rem',
+      }
+    }    
+    
+  },
+  variants: {},
+  plugins: [],
+}
